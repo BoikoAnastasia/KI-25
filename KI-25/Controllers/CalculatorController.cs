c4f9c84a81c892545868af3061e64121fdee1b83
talybov.nm <talybov.nm@3218EC1>
2024-04-02 16:52:18 +0300

    исправил 2 задание

--- KI-25/Controllers/CalculatorController.cs
+++ KI-25/Controllers/CalculatorController.cs
@@ -5,13 +5,22 @@ namespace KI_25.Controllers
 {
     public class CalculatorController : Controller
     {
-        [Route("calculator/index/{num1?}/{num2?}")]
-        public IActionResult Index(double num1 = 0, double num2 = 0)
+        
+        public string Index(int  num1, int num2)
         {
-
-
-            return Content($"{num1}+{num2}= {num1 + num2}", "text/plain");
+            if (num1 == null)
+            {
+                num1 = 0;
+            }
+            if (num2 == null)
+            {
+                num2 = 0;
+            }
+            int num= num1 + num2;
+            string result = num.ToString();
+            return result;
         }
+
 
         
     }
