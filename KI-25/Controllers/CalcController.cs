8a162319d3a57d137d3d2ebd45dbfd7048214658
vladkomlykov <vladalisa04@gmail.com>
2024-04-02 15:58:35 +0300

    сделал калькулятор для сложения, вычетания, деления и умножения

---
+++ KI-25/Controllers/CalcController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(int a, int b, string c)
+        {
+            switch (c)
+            {
+                default:
+                case "+": return a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString();
+                case "-": return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
+                case "*": return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
+                case "/": return a.ToString() + c + b.ToString() + " = " + ((double)a / (double)b).ToString();
+            }
+        }
+    }
+}
