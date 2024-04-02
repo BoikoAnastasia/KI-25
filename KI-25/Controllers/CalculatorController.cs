717e4796dc94f19c7b7bf746b1dd2663061c2def
DasAdErt <misha_tkachev_2018@mail.ru>
2024-04-02 14:28:56 +0300

    Задание 2 с примером выполнено!

---
+++ KI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public String Index(int a, int b)
+        {
+            return a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString();
+        }
+    }
+}
