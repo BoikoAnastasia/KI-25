304b62198210da134fb13e69e1d13c837081a18a
Arisen <havkaset@gmail.com>
2024-04-02 15:00:46 +0300

    task 2 completed

---
+++ KI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string calculate(int firstNum, int secondNum)
+        {
+            return firstNum + " + " + secondNum + " = " + (firstNum + secondNum).ToString();
+        }
+    }
+}
