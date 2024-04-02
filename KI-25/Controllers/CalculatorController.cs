a5cf56d1267e7164255a5bd81be2c76103879320
DasAdErt <misha_tkachev_2018@mail.ru>
2024-04-02 15:07:39 +0300

    Чтобы ошибки не было

--- KI-25/Controllers/CalculatorController.cs
+++ KI-25/Controllers/CalculatorController.cs
@@ -4,7 +4,7 @@ namespace KI_25.Controllers
 {
     public class CalculatorController : Controller
     {
-        public String Index(int a, int b, string c)
+        public String Index1(int a, int b, string c)
         {
             switch (c)
             {
