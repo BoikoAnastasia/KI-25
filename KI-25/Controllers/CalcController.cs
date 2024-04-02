9ef483cf4ef6ca3e42c90c0a4fc87988ccf3f244
DasAdErt <misha_tkachev_2018@mail.ru>
2024-04-02 14:56:38 +0300

    Задание с делением выполнено

---
+++ KI-25/Controllers/CalcController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(int a, int b, string c)
+        {
+            switch (c)
+            {
+                case "+":
+                    return a.ToString() + c + b.ToString() + " = " + (a + b).ToString();
+                case "-":
+                    return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
+                case "*":
+                    return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
+                case "/":
+                    if (b != 0)
+                    {
+                        double result = (double)a / b;
+                        return a.ToString() + c + b.ToString() + " = " + result.ToString();
+                    }
+                    else
+                    {
+                        return "На нуль дилить низя";
+                    }
+            }
+            return "Я ни знаю :((";
+        }
+    }
+}
