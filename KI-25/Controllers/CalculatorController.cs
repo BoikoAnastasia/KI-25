23d6ed47c9e7a37203c76041dbf84f4f8cc1ed17
PapichPapazichNumberOne <kili.molotok@gmail.com>
2024-04-02 23:50:48 +0300

    Выполнил Task2

---
+++ KI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Calculate(string num1, string num2)
+            {
+
+                double number1 = string.IsNullOrEmpty(num1) ? 0 : double.Parse(num1);
+                double number2 = string.IsNullOrEmpty(num2) ? 0 : double.Parse(num2);
+                double result = number1 + number2;
+
+                return $"{num1} + {num2} = {result}";
+            }
+        }
+    }
