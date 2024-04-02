06578e6d0706482f03b47965e82f32c6f1c9d358
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2024-04-02 15:05:15 +0300

    Задание 2

---
+++ KI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    [Route("calculator/index/{a?}/{b?}")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(double a = 0, double b = 0)
+        {
+            return Content($"{a} + {b} = {a + b}", "text/plain");
+        }
+    }
+}
