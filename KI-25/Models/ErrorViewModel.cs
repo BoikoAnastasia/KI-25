75c3a1a52fce2d590e188fae5cf9286a8f24a38a
admin <admin@3218MAIN>
2024-04-02 13:35:30 +0300

    Добавьте файлы проекта.

---
+++ KI-25/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KI_25.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
