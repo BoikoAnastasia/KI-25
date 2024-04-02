fdc15abd6e1832b2b7327834d1da529d0ea47cf2
PapichPapazichNumberOne <kili.molotok@gmail.com>
2024-04-02 23:58:59 +0300

    Calcularot Task3

---
+++ KI-25/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("index/{num1?}/{num2?}/{operation?}")]
+        public ActionResult<string> Calculate(string num1 = "0", string num2 = "0", string operation = "+")
+        { 
+            double number1 = string.IsNullOrEmpty(num1) ? 0 : double.Parse(num1);
+            double number2 = string.IsNullOrEmpty(num2) ? 0 : double.Parse(num2);
+
+            double result;
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                default:
+                    return BadRequest("Неправильная операция. Используйте +, -, *.");
+            }
+
+            return $"{num1} {operation} {num2} = {result}";
+        }
+    }
+}
