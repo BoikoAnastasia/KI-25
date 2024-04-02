d1a5fcb85d6ab56f0c28cb8337a83e41dd608094
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2024-04-02 15:09:51 +0300

    Задание 3

---
+++ KI-25/Controllers/CalculatorControllerOne.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    [Route("calculator/index/{a?}/{b?}/{operation?}")]
+    public class CalculatorControllerOne : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(double a = 0, double b = 0, string operation = "+")
+        {
+            switch (operation)
+            {
+                case "+":
+                    return Content($"{a} + {b} = {a + b}", "text/plain");
+                case "-":
+                    return Content($"{a} - {b} = {a - b}", "text/plain");
+                case "*":
+                    return Content($"{a} * {b} = {a * b}", "text/plain");
+                default:
+                    return Content("Неверная операция. Используйте + для сложения, - для вычитания или * для умножения.", "text/plain");
+            }
+        }
+    }
+}
