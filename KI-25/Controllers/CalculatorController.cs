4661d87b4c8d6d6a31669c886e837787d364bd7b
DasAdErt <misha_tkachev_2018@mail.ru>
2024-04-02 15:27:41 +0300

    13134134

--- KI-25/Controllers/CalculatorController.cs
+++ KI-25/Controllers/CalculatorController.cs
@@ -4,18 +4,9 @@ namespace KI_25.Controllers
 {
     public class CalculatorController : Controller
     {
-        public String Index(int a, int b, string c)
+        public String Index(int a, int b)
         {
-            switch (c)
-            {
-                case "+":
-                    return a.ToString() + c + b.ToString() + " = " + (a + b).ToString();
-                case "-":
-                    return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
-                case "*":
-                    return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
-            }
-            return "Я ни знаю :((";
+            return a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString();
         }
     }
 }
