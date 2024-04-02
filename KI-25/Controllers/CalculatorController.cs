e24fe856ea8efaa2f298d651a60e3b3f3a185cf4
Arisen <havkaset@gmail.com>
2024-04-02 15:10:39 +0300

    task 3 completed

--- KI-25/Controllers/CalculatorController.cs
+++ KI-25/Controllers/CalculatorController.cs
@@ -4,9 +4,25 @@ namespace KI_25.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string calculate(int firstNum, int secondNum)
+        public string calculate(int firstNum, int secondNum, string sign)
         {
-            return firstNum + " + " + secondNum + " = " + (firstNum + secondNum).ToString();
+
+            if (sign.Equals("+"))
+            {
+                return firstNum + " + " + secondNum + " = " + (firstNum + secondNum).ToString();
+            }
+            
+            if (sign.Equals("-"))
+            {
+                return firstNum + " - " + secondNum + " = " + (firstNum - secondNum).ToString();
+            }
+
+            if (sign.Equals("*"))
+            {
+                return firstNum + " * " + secondNum + " = " + (firstNum * secondNum).ToString();
+            }
+
+            return "";
         }
     }
 }
