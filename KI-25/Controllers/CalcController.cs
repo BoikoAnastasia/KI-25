c5a068e25247714d3831f64003577e4dd6ff7aed
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2024-04-05 13:28:12 +0300

    Задание 4

---
+++ KI-25/Controllers/CalcController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class CalcController : Controller
+{
+    public string Index(int a, int b, string c)
+    {
+        switch (c)
+        {
+            default:
+
+            case "+": return a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString();
+            case "-": return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
+            case "*": return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
+            case "/": return a.ToString() + c + b.ToString() + " = " + ((double)a / (double)b).ToString();
+        }
+    }
+}
+}
