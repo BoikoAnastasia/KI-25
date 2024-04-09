a263e2cdac23555334754ca3fba52b96ec17357c
talybov.nm <talybov.nm@3218EC1>
2024-04-09 15:04:45 +0300

    Сделал задание 4

--- KI-25/Controllers/Calc.cs
+++ KI-25/Controllers/Calc.cs
@@ -3,12 +3,48 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace KI_25.Controllers
 {
-    public class Calc : Controller
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CalcController : Controller
     {
-        
-        public string Index()
+        public string Index(double? a, double? b, string c)
         {
-            return ;
+            double numA = a ?? 0;
+            double numB = b ?? 0;
+            string operation = string.IsNullOrEmpty(c) ? "+" : c;
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = numA + numB;
+                    break;
+                case "-":
+                    result = numA - numB;
+                    break;
+                case "*":
+                    result = numA * numB;
+                    break;
+                case "/":
+                    if (numB == 0)
+                        return "Деление на ноль невозможно.";
+
+                    result = numA / numB;
+                    break;
+                default:
+                    return "Неправильно задана операция. Допустимые операции: +, -, *, /";
+            }
+
+            return $"{numA} {operation} {numB} = {result}";
         }
     }
 }
+
+
+
+
+
+
+
