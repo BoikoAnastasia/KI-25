c3b0f906a08ddc1a10b02ed0fce34a5a88641978
talybov.nm <talybov.nm@3218EC1>
2024-04-09 14:23:20 +0300

    Фиксация

---
+++ KI-25/Controllers/Calc.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    public class Calc : Controller
+    {
+        
+        public string Index()
+        {
+            return ;
+        }
+    }
+}
