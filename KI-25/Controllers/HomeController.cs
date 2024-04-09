fffc2a2d2f2a4829c4638be63a0d04d917909825
PapichPapazichNumberOne <kili.molotok@gmail.com>
2024-04-09 13:55:26 +0300

    molotok_lesson_1_6

--- KI-25/Controllers/HomeController.cs
+++ KI-25/Controllers/HomeController.cs
@@ -8,10 +8,10 @@ namespace KI_25.Controllers
     public class HomeController : Controller
     {
         public IList<Product> Items { get; set; }
-        public string Index() {
+        public string Pindex() {
             var str = "";
              var json = System.IO.File.ReadAllText(@"Product.json");
-        Items = JsonConvert.DeserializeObject<IList<Product>>(json);
+            Items = JsonConvert.DeserializeObject<IList<Product>>(json);
         
           
 
@@ -22,7 +22,25 @@ namespace KI_25.Controllers
 
                 return str;
         }
+        public string Index(int id)
+        {
+            string returnStr = "";
+            var json = System.IO.File.ReadAllText(@"Product.json");
+            Items = JsonConvert.DeserializeObject<IList<Product>>(json);
 
-  
+            switch (id)
+            {
+                case 0:
+                    for (int i = 0; i < Items.Count; i++)
+                    {
+                        returnStr += Items[i].Id.ToString() + " " + Items[i].Name.ToString() + " " + Items[i].Price.ToString() + " " + Items[i].Description.ToString() +  Items[i].Category + "\n";
+                    }
+                    return returnStr;
+                default:
+                    return Items[id - 1].Id.ToString() + " " + Items[id - 1].Name.ToString() + " " + Items[id - 1].Price.ToString() + " " + Items[id - 1].Description.ToString() + Items[id - 1].Category ;
+            }
         }
     }
+}
+  
+  
