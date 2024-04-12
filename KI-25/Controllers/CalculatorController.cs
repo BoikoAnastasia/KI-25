8b64e4c0464beaf9569efdf76c0684776f8c27a2
PapichPapazichNumberOne <kili.molotok@gmail.com>
2024-04-12 10:58:36 +0300

    Исправил

--- KI-25/Controllers/CalculatorController.cs
+++ KI-25/Controllers/CalculatorController.cs
@@ -4,37 +4,33 @@ namespace KI_25.Controllers
 {
     public class CalcController : Controller
     {
-        public IActionResult Index(double? a, double? b, string c)
+        public string Index(int a, int b, string c)
         {
-            double numA = a ?? 0;
-            double numB = b ?? 0;
-            string operation = string.IsNullOrEmpty(c) ? "+" : c;
-
-            double result;
-
-            switch (operation)
+            if (a.GetType() == typeof(int) && b.GetType() == typeof(int))
             {
-                case "+":
-                    result = numA + numB;
-                    break;
-                case "-":
-                    result = numA - numB;
-                    break;
-                case "*":
-                    result = numA * numB;
-                    break;
-                case "/":
-         
-                    if (numB == 0)
-                        return BadRequest("Деление на ноль невозможно.");
-
-                    result = numA / numB;
-                    break;
-                default:
-                    return BadRequest("Неправильно задана операция. Допустимые операции: +, -, *, /");
+                switch (c)
+                {
+                    case "+":
+                    default:
+                        return a.ToString() + "+" + b.ToString() + " = " + (a + b).ToString();
+                    case "-":
+                        return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
+                    case "*":
+                        return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
+                    case "/":
+                        if (b != 0)
+                        {
+                            double result = (double)a / b;
+                            return a.ToString() + c + b.ToString() + " = " + result.ToString();
+                        }
+                        else
+                        {
+                            return "На 0 не делется";
+                        }
+                }
+                return "Мы не знаем что это такое";
             }
-
-            return Ok($"{numA} {operation} {numB} = {result}");
+            else return "Неправильно,попробуй еще раз";
         }
     }
 }
