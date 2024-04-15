610ff07b4a9be8f3a38a9491194fe5f50628b6d7
talybov.nm <talybov.nm@3218EC1>
2024-04-15 12:43:12 +0300

    Исправил 3 работу

---
+++ KI-25/Controllers/ValuesController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KI_25.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController1 : ControllerBase
+    {
+    }
+}
