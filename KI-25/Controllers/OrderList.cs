4e074095345b139a79336eb50b2f835a8932d66e
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2024-04-15 15:31:52 +0300

    Task 6

---
+++ KI-25/Controllers/OrderList.cs
@@ -0,0 +1,14 @@
+﻿using KI_25.Controllers.Data;
+
+namespace KI_25.Controllers
+{
+    public static class OrderList
+    {
+        public static List<ShopData> shopDatas = new List<ShopData>() {
+            new ShopData(1, "Premier Item", 100, "Insight on the premier item"),
+            new ShopData(2, "Elite Gear", 200, "Details of the elite gear"),
+            new ShopData(3, "Trio Treasure", 300, "Profile of the trio treasure"),
+            new ShopData(4, "Quartet Bundle", 400, "Overview of the quartet bundle"),
+        };
+    }
+}
