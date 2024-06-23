83f085847c4dac7c7296c4e237c4a1832d95ae64
yoda_ <yoda_@DESKTOP-OPFT5CD>
2024-06-24 01:45:01 +0300

    Выполненпие

--- KI-25/Controllers/CalcController.cs
+++ KI-25/Controllers/CalcController.cs
@@ -5,24 +5,15 @@ namespace CalculatorService.Controllers
     [Route("calc")]
     public class CalcController : Controller
     {
-        
-        [HttpGet("index")]
-        public IActionResult Calculate(double a = 0, double b = 0, string c = "+")
+        public string Index(int a, int b, string c)
         {
             switch (c)
             {
-                case "+":
-                    return Content($"{a} + {b} = {a + b}");
-                case "-":
-                    return Content($"{a} - {b} = {a - b}");
-                case "*":
-                    return Content($"{a} * {b} = {a * b}");
-                case "/":
-                    if (b == 0)
-                        return Content("Ошибка: Деление на ноль невозможно.");
-                    return Content($"{a} / {b} = {a / b}");
                 default:
-                    return Content("Ошибка: Неверная операция. Используйте +, -, * или /.");
+                case "+": return a.ToString() + " + " + b.ToString() + " = " + (a + b).ToString();
+                case "-": return a.ToString() + c + b.ToString() + " = " + (a - b).ToString();
+                case "*": return a.ToString() + c + b.ToString() + " = " + (a * b).ToString();
+                case "/": return a.ToString() + c + b.ToString() + " = " + ((double)a / (double)b).ToString();
             }
         }
     }
