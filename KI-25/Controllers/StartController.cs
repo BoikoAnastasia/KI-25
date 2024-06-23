13e220e3e1911a211b683c3c6a342005cbea4a9a
talyb <talyb@DESKTOP-TE4LVG3>
2024-06-24 01:00:45 +0300

    РАБОТА ВЫПОЛНЕНА

--- KI-25/Controllers/StartController.cs
+++ KI-25/Controllers/StartController.cs
@@ -5,35 +5,33 @@ namespace KI_25.Controllers
 {
     public class StartController : Controller
     {
-        // GET: StartController
-        public string Hello()
+            public string Hello(string time)
+            {
+                DateTime currentTime = DateTime.Now;
+
+                string greeting = GetGreeting(currentTime.Hour);
+
+                return greeting;
+            }
+
+        public string GetGreeting(int hour)
         {
-            
-
-            DateTime currentTime = DateTime.Now;
-            int currentHour = currentTime.Hour;
-
-            if (currentHour >= 0 && currentHour < 6)
+            if (hour >= 0 && hour < 6)
             {
-                return ("Доброй ночи");
+                return "Добрый ночи.";
             }
-            else if (currentHour >= 6 && currentHour < 12)
+            else if (hour >= 6 && hour < 12)
             {
-                return ("Доброе утро");
+                return "Доброе утро.";
             }
-            else if (currentHour >= 12 && currentHour < 18)
+            else if (hour >= 12 && hour < 18)
             {
-                return ("Добрый день");
+                return "Добрый день.";
             }
-            else
-            {
-                return("Добрый вечер");
-            
-            }
-            
+            else { return "Добрый вечер."; }
         }
-
     }
-        
 }
+
+
 
